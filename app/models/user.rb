79a7fa4c1450df21038d4521b4ff5fb14994f794
xgicmt <xgicmt@gmail.com>
2015-05-26 11:57:03 -0500

    corrected typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0,20]
+  	end
   end
 
   def self.new_with_session(params, session)
