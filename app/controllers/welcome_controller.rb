c06b8a10e3edc5f16d53988bc4ef3072fbd0c0ab
xgicmt <xgicmt@gmail.com>
2015-05-27 10:58:37 -0500

    Add jumbotron and frontpage links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+   @imageL = Image.where(landscape: true).order("RANDOM()").first(2)
+   @imageP = Image.where(landscape: nil).order("RANDOM()").first(2)
   end
 
   def about
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -23,4 +23,5 @@ module BoardsHelper
    
   end
  
+
 end
