641aab8eeed8de4b3a1d7f437ef800d9d71a6cf4
Charlie Gaines <beaugaines@gmail.com>
2015-05-29 13:42:31 -0400

    solution for shared board

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-   @imageL = Image.where(landscape: true).order("RANDOM()").first(2)
-   @imageP = Image.where(landscape: nil).order("RANDOM()").first(2)
+    @imageL = Image.where(landscape: true).order("RANDOM()").first(2)
+    @imageP = Image.where(landscape: nil).order("RANDOM()").first(2)
+    @shared_board_id = Board.all.sample.shared_link
   end
 
   def about
